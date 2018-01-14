b195bed2320b3767133ed4449c768436249af9bd
TechMirage <axmaker.nick@gmaill.com>
2018-01-14 15:42:52 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// Basic Resources
+var logs = 0;
+var food = 30;
+
+// Types of Jobs
+var idleVillagers = 4;
+var hunters = 4;
+var loggers = 0;
+
+// Buildings
+var huts = 4; // Start with 4 huts to accommodate your starting tribesman/woman.
+
+// Technology Multipliers
+var huntGatherMultiplier = 2;
+
+// What happens when you click the gathering buttons? 
+
+function logsClick(number){
+    logs = logs + number;
+    document.getElementById("logs").innerHTML = logs;
+};
+
+function foodClick(number){
+    food = food + number;
+    document.getElementById("food").innerHTML = food;
+};
+
+var cursors = 0;
+
+function buyHut(){
+    var hutCost = Math.floor(10 * Math.pow(1.1,(huts-4)));     	// (don't count the first four of the playthrough)
+    if(logs >= hutCost){                                   		
+        huts = huts + 1;                                   
+    	logs = logs - hutCost;                          			
+        document.getElementById('huts').innerHTML = huts;  	
+        document.getElementById('logs').innerHTML = logs;  			
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,huts));       
+    document.getElementById('hutCost').innerHTML = nextCost;  
+};
+
+function assignWorker(job, number){
+	if ((idleVillagers - number) >= 0){
+		if(job === 'loggers' && (loggers + number) >= 0){
+			idleVillagers = idleVillagers - number;
+			loggers = loggers + number;
+			document.getElementById('loggers').innerHTML = loggers;
+		}
+		else if(job == 'hunters' && (hunters + number) >= 0) {
+			idleVillagers = idleVillagers - number;
+			hunters = hunters + number;
+			document.getElementById('hunters').innerHTML = hunters;
+		}
+		document.getElementById('idleVillagers').innerHTML = idleVillagers;
+	}
+
+}
+
+function totalVillagers() {
+	return idleVillagers + hunters + loggers;
+}
+
+window.setInterval(function(){
+	
+	logsClick(loggers);
+	foodClick((hunters*huntGatherMultiplier) - totalVillagers());
+	
+}, 1000);
