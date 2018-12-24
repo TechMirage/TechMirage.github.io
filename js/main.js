4bdce6e9d96af524da13677d0af04cf46b459fb7
TechMirage <27571973+TechMirage@users.noreply.github.com>
2018-12-23 21:06:24 -0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// TODO:
+//  - Add a reset button for erasing your saves.
+//  - Add more content (jobs, buildings, research, etc)
+//  - Fix the layout issues and see about making borders/aesthetics
+//  - Put all the updates to the numbers on screen into one function and
+//      call it in the main game loop after the other updates, rather than
+//      have all the document refresh calls in each individual function.
+//  - Add a message log in the main game window that gives updates
+//      as they are received (people dying, milestones achieved, etc.)
+
 // Basic Resources
 var logs = 0;
 var food = 30;
@@ -78,32 +88,29 @@ function assignWorker(job, number){
 
 // As it turns out, villagers can't live without food and will start dying.
 // Priority list for Death: Idle > non-food workers > food gatherers
+
+// TODO: Fix this entire function (ie replace it with something that actually works)
+// TODO: Also need to figure out a better way to kill off all the jobs.
 function killVillagers(number){
   var numToDie = number;
   if (idleVillagers > 0) {
-    console.log('idle villagers to kill')
     if (numToDie - idleVillagers > 0) {
       idleVillagers = idleVillagers - numToDie;
     } else {
-      console.log('not enough though' + numToDie)
       numToDie = numToDie - idleVillagers;
       idleVillagers = 0;
     }
   } else if (numToDie > 0 && loggers > 0) {
-    console.log('loggers to kill')
     if (numToDie - loggers > 0) {
       loggers = loggers - numToDie;
     } else {
-      console.log('not enough though')
       numToDie = numToDie - loggers;
       loggers = 0;
     }
   } else if (numToDie > 0 && hunters > 0) {
-    console.log('hunters to kill')
     if (numToDie - hunters > 0) {
       hunters = hunters - numToDie;
     } else {
-      console.log('not enough though????')
       numToDie = numToDie - hunters;
       hunters = 0;
     }
@@ -131,4 +138,5 @@ window.setInterval(function(){
 	foodClick((hunters*huntGatherMultiplier) - totalVillagers());
 	growVillagers();
 	
+	// TODO: Save the game to a local cookie
 }, 1000);
